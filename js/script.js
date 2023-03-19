a8c02e975bd9f30677ffed1f63b507f38b5b8b8b
Eleanor Gakii <ellegakii@gmail.com>
2023-03-19 14:49:09 +0300

    Added Swiper slider, search functionality and displayed search results

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const global = {
     currentPage: window.location.pathname,
+    search: {
+        term: '',
+        type: '',
+        page: 1,
+        totalPages: 1,
+    },
+    api: {
+        apiKey: '769c5ae3c4828db8f4369db378cefae6',
+        apiUrl: 'https://api.themoviedb.org/3'
+    }
+
 };
 
 async function displayPopularMovies() {
@@ -212,19 +223,147 @@ function displayBackgroundImage(type, backgroundPath) {
     } else {
         document.querySelector('#show-details').appendChild(overlayDiv)
     }
+}
 
+//Search Movies/shows
 
+async function search() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString)
+
+    global.search.type = urlParams.get('type')
+    global.search.term = urlParams.get('search-term')
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        // make request and display results
+        const { results, total_pages, page } = await searchAPIData()
+
+        if (results.length === 0) {
+            showAlert('No results found')
+            return;
+        }
+
+        displaySearchResults(results)
+
+        document.querySelector('#search-term').value = '';
+    } else {
+        showAlert('Please enter a search term')
+    }
+}
+
+function displaySearchResults(results) {
+    results.forEach((result) => {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+        <a href="${global.search.type}-details.html?id=${result.id}">
+        ${result.poster_path
+                ? `<img
+            src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+            class="card-img-top"alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-image-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+            }
+            </a>
+            <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+            <small class="text-muted">
+            Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+            </div>
+            `;
+
+        document.querySelector('#search-results').appendChild(div);
+    })
+}
+
+//Display Slider Movies
+
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing')
+
+    results.forEach((movie) => {
+        const div = document.createElement('div')
+        div.classList.add('swiper-slide')
+        div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    </a>
+    <h4 class="Swiper-rating">
+    <i class="fas fa-star text-secondary"></i>
+    ${movie.vote_average} / 10
+    </h4>
+    `;
+
+        document.querySelector('.swiper-wrapper').appendChild(div)
+        initSwiper()
+    })
+}
+
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2
+            },
+            700: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            },
+        }
+
+    })
 }
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-    const API_KEY = '769c5ae3c4828db8f4369db378cefae6';
-    const API_URL = 'https://api.themoviedb.org/3';
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
 
     showSpinner()
 
     const response = await fetch(
         `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+
+    hideSpinner()
+
+    return data;
+}
+
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show')
+}
+
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show')
+}
+
+
+// Make request to search
+async function searchAPIData() {
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
+
+    showSpinner()
+
+    const response = await fetch(
+        `${API_URL}/search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
     const data = await response.json();
 
     hideSpinner()
@@ -251,6 +390,17 @@ function highlightActiveLink() {
     })
 }
 
+//Show Error
+
+function showAlert(message, className = 'error') {
+    const alertEl = document.createElement('div')
+    alertEl.classList.add('alert', className)
+    alertEl.appendChild(document.createTextNode(message))
+    document.querySelector('#alert').appendChild(alertEl)
+
+    setTimeout(() => alertEl.remove(), 3000)
+}
+
 function addCommasToNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -260,6 +410,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider()
             displayPopularMovies();
             break;
         case '/shows.html':
@@ -272,7 +423,7 @@ function init() {
             displayShowDetails()
             break;
         case '/search.html':
-            console.log('Search')
+            search()
             break;
 
     }
